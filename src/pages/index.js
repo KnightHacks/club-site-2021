e39578e275261338e02cfc631eb943c845ae70ec
Robert Boyd III <rob@robertcboyd.com>
2021-02-12 18:43:08 -0500

    Fix fontsource roboto error

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Newsletter from "../components/newsletter.js";
 import Navbar from "../components/navbar.js";
 import Calendar from "../components/calendar.js";
 import "./index.css";
-import "fontsource-roboto";
+import "@fontsource/roboto";
 
 const logoHeight = 100;
 const logoWidth = 200;
