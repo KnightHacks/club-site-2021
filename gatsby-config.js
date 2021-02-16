275f8baf72e840cac21873ae4da30b385ba33a48
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-02-16 00:54:17 -0500

    Sets up project to read Markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: "KnightHacks Club Site",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
