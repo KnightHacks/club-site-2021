995bac7f78954da3baf8b1189e473c7e603288c4
aileen <akendrap@gmail.com>
2021-02-17 23:14:50 -0500

    adjust margin & include card hover

--- src/components/events.js
+++ src/components/events.js
@@ -2,38 +2,42 @@ import React, { useState } from "react";
 import Accordion from '@material-ui/core/Accordion';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import "./events.css";
 
 const Events = () => {
-  //
-  // const [expanded, setExpanded] = React.useState('panel1');
-  //
-  // const handleChange = (panel) => (event, isExpanded) => {
-  //   setExpanded(isExpanded ? panel : false);
-  // };
+
+  const [expanded, setExpanded] = React.useState('panel1');
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   return (
     <div className="CalendarContainer">
       <Accordion
       defaultExpanded
-      // expanded={expanded === 'panel1'} onChange={handleChange('panel1')}
       className="AccordionCard"
+      id="panel1bh"
       >
         <AccordionSummary
+          className="AccordionHeader"
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
           <h1 className="Heading">Event Title</h1>
         </AccordionSummary>
-        <AccordionDetails>
+        <AccordionDetails
+        className="AccordionContent"
+        >
           <h3 className="Secondary">
             Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
             maximus est, id dignissim quam.
           </h3>
         </AccordionDetails>
       </Accordion>
-      <Accordion defaultExpanded
+      <Accordion
+      defaultExpanded
+      // expanded={expanded === 'panel2'} onChange={handleChange('panel2')}
       className="AccordionCard">
         <AccordionSummary
           aria-controls="panel2bh-content"
