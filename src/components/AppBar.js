796dad1b9b6b1f0faca7bac681cc0657928c7b46
Robert Boyd III <rob@robertcboyd.com>
2021-02-18 22:15:24 -0500

    Add transparency effect to AppBar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { AppBar as MaterialAppBar, Toolbar, Button } from "@material-ui/core";
+import {
+  AppBar as MaterialAppBar,
+  Toolbar,
+  Button,
+  useScrollTrigger,
+} from "@material-ui/core";
 import "./AppBar.css";
 
 const AppBar = () => {
-  return (
-    <MaterialAppBar position="sticky">
-      <Toolbar className="appbar">
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(
+    <MaterialAppBar
+      className={trigger ? "opaque" : "transparent"}
+      position="sticky"
+    >
+      <Toolbar className="buttons">
         <Button color="inherit">About</Button>
         <Button color="inherit">Events</Button>
         <Button color="inherit">Team</Button>
         <Button color="inherit">Contact Us</Button>
       </Toolbar>
-    </MaterialAppBar>
+    </MaterialAppBar>,
+    { elevation: trigger ? 10 : 0 }
   );
 };
 
