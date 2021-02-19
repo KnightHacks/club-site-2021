b180fdf241ed2c9436a041fc55124052c46f5155
aileen <akendrap@gmail.com>
2021-02-18 23:50:43 -0500

    fixed opposing navbar links

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,42 +1,20 @@
 import React from "react";
-import {
-  AppBar as MaterialAppBar,
-  Toolbar,
-  Button,
-  ButtonGroup,
-  useScrollTrigger,
-} from "@material-ui/core";
+import { AppBar as MaterialAppBar, Toolbar, Button, ButtonGroup } from "@material-ui/core";
 import "./AppBar.css";
 
 const AppBar = () => {
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-  });
-
-  return React.cloneElement(
-    <MaterialAppBar
-      className={trigger ? "opaque" : "transparent"}
-      position="fixed"
-    >
+  return (
+    <MaterialAppBar position="sticky">
       <Toolbar className="buttons">
-        <ButtonGroup variant="text">
-          <Button className="button" color="inherit">
-            About
-          </Button>
-          <Button className="button" color="inherit">
-            Events
-          </Button>
-          <Button className="button" color="inherit">
-            Team
-          </Button>
-          <Button className="button" color="inherit">
-            Contact Us
-          </Button>
-        </ButtonGroup>
+      <Button color="inherit" className = "tree"> Linktree </Button>
+      <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+        <Button className = "button" color="inherit">About</Button>
+        <Button className = "button" color="inherit">Events</Button>
+        <Button className = "button" color="inherit">Team</Button>
+        <Button className = "button" color="inherit">Contact Us</Button>
+      </ButtonGroup>
       </Toolbar>
-    </MaterialAppBar>,
-    { elevation: trigger ? 10 : 0 }
+    </MaterialAppBar>
   );
 };
 
