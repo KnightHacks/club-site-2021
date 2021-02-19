4b8458d0fb80b542daae85e8e2aeda7b9af438c1
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-02-18 23:23:13 -0500

    Fixes major z-index bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import KnightHacksLogo from "../assets/logos/knightHacksLogoGold.svg";
 import Newsletter from "../components/newsletter.js";
 import Contacts from "../components/contacts.js";
 import Calendar from "../components/calendar.js";
-import Particles from "react-particles-js";
+import ReactParticles from "react-particles-js";
 import particles_config from "../particles-config";
 import "./index.css";
 import "@fontsource/roboto";
@@ -16,27 +16,45 @@ const logoWidth = 200;
 const IndexPage = () => {
   return (
     <div className="LandingPage">
-      <div className="ParticlesContainer">
-        <Particles className="particles" params={{ ...particles_config }} />
-      </div>
-      <title>Home Page</title>
-      <div className="KnightHacksLogo">
-        <img
-          src={KnightHacksLogo}
-          className="KHLogo"
-          height={logoHeight}
-          width={logoWidth}
-          alt="Knight Hacks Logo"
-        />
-      </div>
-      <Calendar></Calendar>
-      <div className="FooterContainer">
-        <h1 className="Subtitle">Connect With Us</h1>
-        <div className="Footer">
-          <Newsletter></Newsletter>
-          <Contacts></Contacts>
+      <Particles className="particles" params={{ ...particles_config }}>
+        <title>Home Page</title>
+        <div className="KnightHacksLogo">
+          <img
+            src={KnightHacksLogo}
+            className="KHLogo"
+            height={logoHeight}
+            width={logoWidth}
+            alt="Knight Hacks Logo"
+          />
         </div>
-      </div>
+        <Calendar></Calendar>
+        <div className="FooterContainer">
+          <h1 className="Subtitle">Connect With Us</h1>
+          <div className="Footer">
+            <Newsletter></Newsletter>
+            <Contacts></Contacts>
+          </div>
+        </div>
+      </Particles>
+    </div>
+  );
+};
+
+const Particles = ({ children }) => {
+  return (
+    <div style={{ position: "relative" }}>
+      <ReactParticles
+        params={particles_config}
+        style={{
+          position: "absolute",
+          zIndex: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          top: 0,
+        }}
+      />
+      {children && <div style={{ position: "relative" }}>{children}</div>}
     </div>
   );
 };
