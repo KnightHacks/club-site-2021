99316cfaaee3af595e2fd6d8d372b8b6341ae713
Leah Howells <65057296+leahhowells@users.noreply.github.com>
2021-02-22 17:47:54 -0500

    Add missing <StylesProvider injectFirst> opening tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import "../fonts/avenirnext-ultralight.ttf";
 
 const IndexPage = () => {
   return (
+    <StylesProvider injectFirst>
       <AppBar />
       <div className="LandingPage">
         <Particles>
