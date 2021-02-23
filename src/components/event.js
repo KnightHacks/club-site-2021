0799bdca4e01748c2ec9dcf760025678e7bc8397
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-02-22 19:50:34 -0500

    Fixes no expanded margin for accordions

--- src/components/event.js
+++ src/components/event.js
@@ -1,42 +1,39 @@
-import React from "react";
-import Accordion from '@material-ui/core/Accordion';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
+import React, { useState } from "react";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
 import "./event.css";
 
 const Events = (props) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <div className="CalendarContainer">
+    <div className={`CalendarContainer ${expanded ? "Expanded" : ""}`}>
       <Accordion
-      className="AccordionCard"
-      id="panel1bh"
+        className="AccordionCard"
+        id="panel1bh"
+        onChange={() => setExpanded(!expanded)}
       >
         <AccordionSummary
           className="AccordionHeader"
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-        <div className="AccordionSchedule">
-          <h1 className="EventTitle"> {props.title} </h1>
-          <span className="EventDate">
-            {props.date}
-            <span className="EventMonth">
-            {props.month}
+          <div className="AccordionSchedule">
+            <h1 className="EventTitle"> {props.title} </h1>
+            <span className="EventDate">
+              {props.date}
+              <span className="EventMonth">{props.month}</span>
             </span>
-          </span>
-        </div>
+          </div>
         </AccordionSummary>
-        <AccordionDetails
-        className="AccordionContent"
-        >
-        <div className="ContentContainer">
-          <p className="EventTime">
-          {props.time}   |   {props.location}
-          </p>
-          <h3 className="EventDescription">
-            {props.description}
-          </h3>
-        </div>
+        <AccordionDetails className="AccordionContent">
+          <div className="ContentContainer">
+            <p className="EventTime">
+              {props.time} | {props.location}
+            </p>
+            <h3 className="EventDescription">{props.description}</h3>
+          </div>
         </AccordionDetails>
       </Accordion>
     </div>
