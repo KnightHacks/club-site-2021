af41292db823dd33b63d3acee00e9007ef3286b8
Abraham Hernandez <habraham@fb.com>
2021-02-28 23:27:33 -0500

    Dynamically generate redirect links, also makes 404.js look nicer

---
+++ gatsby-node.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    query redirectsQuery {
+      markdownRemark(frontmatter: { title: { eq: "Redirects" } }) {
+        frontmatter {
+          redirects {
+            discord
+            zoom
+            vote
+            twitter
+            slack
+            resume
+            ops
+            membership
+            marketing
+            linktree
+            instagram
+            feedback
+            facebook
+            dues
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  console.log(result.data.markdownRemark.frontmatter.redirects);
+  const redirectTemplate = path.resolve(`src/templates/redirect.js`);
+  for (const [name, link] of Object.entries(
+    result.data.markdownRemark.frontmatter.redirects
+  )) {
+    createPage({
+      path: name,
+      component: redirectTemplate,
+      context: {
+        title: name,
+        redirect: link,
+      },
+    });
+  }
+};
---
+++ src/markdown-pages/redirects.md
@@ -0,0 +1,21 @@
+---
+title: "Redirects"
+
+redirects:
+  {
+    "slack": "https://knighthacks.slack.com",
+    "facebook": "https://www.facebook.com/KnightHacks/",
+    "discord": "https://discord.gg/Kv5g9vf",
+    "instagram": "https://www.instagram.com/KnightHacks/",
+    "twitter": "https://www.twitter.com/KnightHacks/",
+    "marketing": "https://drive.google.com/open?id=1JfvcumDOMlS4YQuNS-6NvCh2cn52JrA2",
+    "resume": "https://forms.gle/p2TbagqardN9MPfC6",
+    "linktree": "https://linktr.ee/knighthacks",
+    "zoom": "https://ucf.zoom.us/j/95272477189?pwd=TlliV1gwRmxRajdzMmJ4N1ZhRmp4Zz09",
+    "membership": "https://forms.gle/5kA8gwazBQA1ei8z6",
+    "dues": "https://checkout.square.site/pay/864a3d6631a44a15aeebd65104a6f9e1",
+    "ops": "https://ucf.zoom.us/j/98281000067?pwd=TzNWRTdmMmc2TUk4MElSREhLRGp1UT09",
+    "feedback": "https://forms.gle/EfMQnpxb6SefA4tH7",
+    "vote": "https://forms.gle/KaefRH5FZuX3kZ789",
+  }
+---
