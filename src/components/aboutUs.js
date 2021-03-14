5eb37f0bb8a12ff8d9fc0cb18bf321151a85574d
Ro-Hanna Jowallah <rohannajowallah@gmail.com>
2021-03-14 14:11:08 -0400

    about us text converted to twcss

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -4,7 +4,7 @@ import "./aboutUs.css";
 
 const AboutUs = forwardRef((props, ref) => {
   return (
-    <div className="AboutUsContainer" ref={ref}>
+    <div className="flex flex-col justify-center w-full text-center mt-14 mb-10" ref={ref}>
       <h1 className="font-light flex justify-center text-gray-50 text-5xl w-full my-5">
         About Us
       </h1>
@@ -17,7 +17,7 @@ const AboutUs = forwardRef((props, ref) => {
           }
         `}
         render={(data) => (
-          <p className="AboutUsText">{data.markdownRemark.rawMarkdownBody}</p>
+          <p className="text-gray-300 text-2xl leading-loose ml-28 mr-28">{data.markdownRemark.rawMarkdownBody}</p>
         )}
       />
     </div>
