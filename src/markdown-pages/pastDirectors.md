dba55d60a6f2280221b5b9f8746d21e554d3d1a4
Ro-Hanna Jowallah <rohannajowallah@gmail.com>
2021-04-05 10:34:55 -0400

    Redirect/ Markdown page

---
+++ src/markdown-pages/pastDirectors.md
@@ -0,0 +1,54 @@
+---
+title: "Directors"
+members:
+  [
+    {
+      name: "",
+      position: "",
+      contributions: "",
+      image: "",
+      linkedin: "",
+      instagram: "",
+    },
+    {
+      name: "",
+      position: "",
+      contributions: "",
+      image: "",
+      linkedin: "",
+      instagram: "",
+    },
+    {
+      name: "",
+      position: "",
+      contributions: "",
+      image: "",
+      linkedin: "",
+      instagram: "",
+    },
+    {
+      name: "",
+      position: "",
+      contributions: "",
+      image: "",
+      linkedin: "",
+      instagram: "",
+    },
+    {
+      name: "",
+      position: "",
+      contributions: "",
+      image: "",
+      linkedin: "",
+      instagram: "",
+    },
+    {
+      name: "",
+      position: "",
+      contributions: "",
+      image: "",
+      linkedin: "",
+      instagram: "",
+    },
+  ]
+---
--- src/pages/directors.js
+++ src/pages/directors.js
@@ -5,6 +5,7 @@ import ReactParticles from "react-particles-js";
 import particles_config from "../particles-config";
 import "./index.css";
 import { Grid } from "@material-ui/core";
+import { Route, BrowserRouter as Router, Link, Switch } from "react-router-dom";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -14,106 +15,119 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import index from "../../src/pages/index.js";
 
 const directors = ({ data }) => {
   return (
-    <StylesProvider injectFirst>
-      <Particles>
-        <h1 className="font-light flex justify-center mt-24 text-gray-50 text-4xl my-5 ml-6 lg:text-5xl">
-          Past Directors
-        </h1>
-        <div className="mt-32 ml-36 justify-center">
-          <Grid container spacing={6}>
-            <Grid item>
-              <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    alt="Student Image"
-                    height="140"
-                    image="../../images/abr.jpg"
-                    title="Student Image"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      First Last
-                    </Typography>
+    <Router>
+      <StylesProvider injectFirst>
+        <Particles>
+          <Link to="/index">
+            <Button variant="contained" color="primary" className="ml-9 mt-9">
+              Back to Home
+            </Button>
+          </Link>
+          <Switch>
+            <Route path="/index" component={index} exact={true} />
+          </Switch>
+          <h1 className="font-light flex justify-center mt-24 text-gray-50 text-4xl my-5 ml-6 lg:text-5xl">
+            Past Directors
+          </h1>
+
+          <div className="mt-32 ml-36 justify-center">
+            <Grid container spacing={6}>
+              <Grid item>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      alt="Student Image"
+                      height="140"
+                      image="../../images/abr.jpg"
+                      title="Student Image"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        First Last
+                      </Typography>
+
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                      >
+                        A summary of the contributions that were made to the
+                        club
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <a>
+                      <FontAwesomeIcon
+                        icon={faLinkedin}
+                        color="#000000"
+                        className="mr-2 pr-0.5 pl-2 mt-0 text-4xl hover:text-gray-400"
+                      />
+                    </a>
+                    <a>
+                      <FontAwesomeIcon
+                        icon={faInstagram}
+                        color="#000000"
+                        className="mr-2 pr-0.5 pl-2 text-4xl hover:text-gray-400"
+                      />
+                    </a>
 
                     <Typography
-                      variant="body2"
+                      className="pl-1 pb-1.5 text-base"
+                      align="left"
+                      variant="h2"
                       color="textSecondary"
                       component="p"
                     >
-                      A summary of the contributions that were made to the club
+                      Years Active
                     </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <a>
-                    <FontAwesomeIcon
-                      icon={faLinkedin}
-                      color="#000000"
-                      className="mr-2 pr-0.5 pl-2 mt-0 text-4xl hover:text-gray-400"
-                    />
-                  </a>
-                  <a>
-                    <FontAwesomeIcon
-                      icon={faInstagram}
-                      color="#000000"
-                      className="mr-2 pr-0.5 pl-2 text-4xl hover:text-gray-400"
-                    />
-                  </a>
-
-                  <Typography
-                    className="pl-1 pb-1.5 text-base"
-                    align="left"
-                    variant="h2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    Years Active
-                  </Typography>
-                </CardActions>
-              </Card>
-              );
+                  </CardActions>
+                </Card>
+                );
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
+              <Grid item>
+                <Card />
+              </Grid>
             </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-            <Grid item>
-              <Card />
-            </Grid>
-          </Grid>
-        </div>
-      </Particles>
-    </StylesProvider>
+          </div>
+        </Particles>
+      </StylesProvider>
+    </Router>
   );
 };
 
