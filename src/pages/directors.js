27374cd119b9a1466f3680973441a9730a0bb6d8
Ro-Hanna Jowallah <rohannajowallah@gmail.com>
2021-04-05 09:06:55 -0400

    Removing Navbar

--- src/pages/directors.js
+++ src/pages/directors.js
@@ -3,34 +3,79 @@ import AppBar from "../components/appBar.js";
 import { StylesProvider } from "@material-ui/core/styles";
 import ReactParticles from "react-particles-js";
 import particles_config from "../particles-config";
-import Card from "../components/card.js";
 import "./index.css";
 import { Grid } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const directors = ({ data }) => {
-  const appBarRef = useRef(null);
-  const aboutUsRef = useRef(null);
-  const eventsRef = useRef(null);
-  const teamsRef = useRef(null);
-  const contactUsRef = useRef(null);
   return (
     <StylesProvider injectFirst>
-      <AppBar
-        ref={appBarRef}
-        appBarRef={appBarRef}
-        aboutUsRef={aboutUsRef}
-        eventsRef={eventsRef}
-        teamsRef={teamsRef}
-        contactUsRef={contactUsRef}
-      />
       <Particles>
         <h1 className="font-light flex justify-center mt-24 text-gray-50 text-4xl my-5 ml-6 lg:text-5xl">
           Past Directors
         </h1>
         <div className="mt-32 ml-36 justify-center">
-          <Grid container spacing={7}>
+          <Grid container spacing={6}>
             <Grid item>
-              <Card />
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Student Image"
+                    height="140"
+                    image="../../images/abr.jpg"
+                    title="Student Image"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      First Last
+                    </Typography>
+
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      A summary of the contributions that were made to the club
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <a>
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      color="#000000"
+                      className="mr-2 pr-0.5 pl-2 mt-0 text-4xl hover:text-gray-400"
+                    />
+                  </a>
+                  <a>
+                    <FontAwesomeIcon
+                      icon={faInstagram}
+                      color="#000000"
+                      className="mr-2 pr-0.5 pl-2 text-4xl hover:text-gray-400"
+                    />
+                  </a>
+
+                  <Typography
+                    className="pl-1 pb-1.5 text-base"
+                    align="left"
+                    variant="h2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Years Active
+                  </Typography>
+                </CardActions>
+              </Card>
+              );
             </Grid>
             <Grid item>
               <Card />
