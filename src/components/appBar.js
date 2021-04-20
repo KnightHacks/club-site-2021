190bd62329c791a347a135623fe0bca7da0b0791
Robert Boyd III <rob@robertcboyd.com>
2021-04-20 19:34:10 -0400

    Tweak appBar to accommodate accessibility changes

--- src/components/appBar.js
+++ src/components/appBar.js
@@ -60,7 +60,7 @@ const AppBar = forwardRef(
     const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
-      if (width > 600) {
+      if (width > 700) {
         setIsOpen(false);
       }
     }, [width]);
@@ -78,12 +78,12 @@ const AppBar = forwardRef(
       >
         <div className="flex flex-row flex-nowrap overflow-hidden items-center justify-between xs:text-base sm:text-lg md:text-xl">
           <AppBarLink
-            className={isOpen ? "flex-auto" : ""}
+            className={isOpen ? "flex-auto text-left" : ""}
             href="https://linktr.ee/knighthacks"
           >
             Linktree
           </AppBarLink>
-          <div className={width <= 600 ? "hidden" : "visible"}>
+          <div className={width <= 700 ? "hidden" : "visible"}>
             <AppBarLink onClick={() => scroll(aboutUsRef)}>About</AppBarLink>
             <AppBarLink onClick={() => scroll(eventsRef)}>Events</AppBarLink>
             <AppBarLink onClick={() => scroll(teamsRef)}>Team</AppBarLink>
@@ -93,7 +93,7 @@ const AppBar = forwardRef(
           </div>
           <div
             className={
-              width > 600
+              width > 700
                 ? "hidden"
                 : "visible" + " flex flex-col items-center justify-center"
             }
@@ -120,6 +120,7 @@ const AppBar = forwardRef(
           className="overflow-hidden transition-all flex flex-col"
         >
           <AppBarLink
+            className="text-left"
             onClick={() => {
               setIsOpen(false);
               scroll(aboutUsRef);
@@ -128,6 +129,7 @@ const AppBar = forwardRef(
             About
           </AppBarLink>
           <AppBarLink
+            className="text-left"
             onClick={() => {
               setIsOpen(false);
               scroll(eventsRef);
@@ -136,6 +138,7 @@ const AppBar = forwardRef(
             Events
           </AppBarLink>
           <AppBarLink
+            className="text-left"
             onClick={() => {
               setIsOpen(false);
               scroll(teamsRef);
@@ -144,6 +147,7 @@ const AppBar = forwardRef(
             Team
           </AppBarLink>
           <AppBarLink
+            className="text-left"
             onClick={() => {
               setIsOpen(false);
               scroll(contactUsRef);
