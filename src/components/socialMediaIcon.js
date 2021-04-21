5ea84e3abcb7f664d4bffc51e93bd72b12ccf413
Robert Boyd III <rob@robertcboyd.com>
2021-04-20 22:21:41 -0400

    Refactor SocialMediaIcon

--- src/components/socialMediaIcon.js
+++ src/components/socialMediaIcon.js
@@ -2,10 +2,10 @@ import * as React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const SocialMediaIcon = ({ href, icon, color, className }) => {
+const SocialMediaIcon = ({ href, ...props }) => {
   return (
     <a href={href}>
-      <FontAwesomeIcon icon={icon} color={color} className={className} />
+      <FontAwesomeIcon {...props} />
     </a>
   );
 };
