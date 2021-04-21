604bc4e416328376a560e5604ec399656f5ffbc9
Robert Boyd III <rob@robertcboyd.com>
2021-04-20 21:32:15 -0400

    Add newline

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
 import FadeIn from "react-fade-in";
+
 const allEvents = [
   {
     title: "GBM and Project Showcase",
