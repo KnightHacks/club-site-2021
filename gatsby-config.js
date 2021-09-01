618d11ae0d03d9e2888469b11673695f49dc6241
aileen <akendrap@gmail.com>
2021-09-01 14:21:06 -0400

    include images subdirectory

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images/`,
+        path: `${__dirname}/src/images/current`,
       },
     },
     `gatsby-transformer-remark`,
